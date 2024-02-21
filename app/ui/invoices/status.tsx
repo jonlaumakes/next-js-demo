3e5a480d39a04c16e226d86e4649703ce289efc0
jonlau <jon.lau.makes@gmail.com>
2024-02-21 13:03:41 -0500

    child routes/layouts added

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,6 +1,8 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+export const inter = Inter({subsets: ['latin']});
+
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
